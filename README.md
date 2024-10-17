55e976f4fb5fcac3b25e7154e544914a062487c6
Shafin Ahmed <79255498+ahshafin2315@users.noreply.github.com>
2024-10-17 17:29:35 +0600

    Newest Update that allows choosing sites
    Just have to input 1 or 2 in prompt to select and the script handles everything else.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Class Availability Checker
 
-This Python script checks for seat availability in real time for a specific class section from BRAC university USIS website and sends a notification to running device when seats are available. The script works on both desktop environments (Windows/Linux) and Android devices through the Termux terminal.
+This Python script checks for seat availability in real time for a specific course section from either the [BRAC University USIS](https://usis.bracu.ac.bd/academia/admissionRequirement/getAvailableSeatStatus) website or [Eniamza's Usis2Global](https://github.com/Eniamza/usis2global) and sends a notification to running device when seats are available. The script works on both desktop environments (Windows/Linux) and Android devices through the Termux terminal.
 
 ## Features
 - Fetches real-time class schedule data from a JSON file.
 - Checks seat availability for a specific course and section.
+- Option to choose seat update from which site preferred.
 - Sends notifications:
   - Desktop notification (Windows/Linux).
   - Termux notification (Android).
@@ -23,6 +24,7 @@ This Python script checks for seat availability in real time for a specific clas
   - `requests`
   - `plyer`
   - `schedule`
+  - `bs4`
 
 ## Installation and Setup
 
@@ -37,10 +39,6 @@ or, just download the `usis_seatCheck.py` script and store in desired location
 
 ### 2. Install dependencies:
 
-```bash
-pip install requests schedule plyer
-```
-
 For Android (Termux):
 Install Python:
 ```bash
@@ -51,13 +49,17 @@ Install Termux API:
 pkg install termux-api
 ```
 
+```bash
+pip install requests schedule plyer bs4
+```
+
 ### 3. Set Up & Run the Script
 Run below code inside cloned script directory:
 
 ```bash
 python usis_seatCheck.py
 ```
-
+Set your desired inputs in the prompts and choose the site you wish to get updated from.
 When a seat becomes available, Desktop or Termux will send you a notification through the system.
 
 ## Scheduled Check
@@ -72,10 +74,10 @@ To adjust, change the number 1 to any number of minutes you prefer.
 Feel free to submit issues or contribute to this project via GitHub. All contributions are welcome!
 
 ## Disclaimer 
-Unfortunately, the [usisdump](https://usis-cdn.eniamza.com/old-usisdump.json) of [Eniamza](https://github.com/Eniamza/) has stopped updating due to data freeze in main USIS database. So, the script won't work as expected 🥲.
+Unfortunately, the usisdump of Eniamza has stopped updating due to data freeze in main USIS database or something (I donno :3). For now, the main USIS site seems to be most updated. So, sorry for the inconvenience if the script data won't update as expected 🥲.
 
 ## Credit
-Huge Thanks to [Eniamza](https://github.com/Eniamza/)'s own [old USIS-CDN](https://usis-cdn.eniamza.com/old-usisdump.json) dump file for my data pulling.
+Huge Thanks to [Eniamza](https://github.com/Eniamza/) and his [USIS-CDN](https://usis-cdn.eniamza.com/old-usisdump.json) dump file for my data pulling.
 
 ## License
 This project is licensed under the Apache 2.0 License - see the LICENSE file for details.
--- usis_seatCheck.py
+++ usis_seatCheck.py
@@ -4,6 +4,7 @@ import os
 import requests
 import schedule
 from plyer import notification
+from bs4 import BeautifulSoup
 
 def clear_scrn():
     if os.name == "nt":
@@ -11,48 +12,79 @@ def clear_scrn():
     else:
         os.system("clear")
 
-# URL for the JSON file
+# URLs for seat status
 json_url = "https://usis-cdn.eniamza.com/old-usisdump.json"
+usis_url = "https://usis.bracu.ac.bd/academia/admissionRequirement/getAvailableSeatStatus"
 
 # Function to check for class availability in the JSON data
 def check_class():
-    global c_code, sec
+    global c_code, sec, url_selected
+
     try:
         print("Fetching results!!!\n")
-        response = requests.get(json_url)
-        response.raise_for_status()  # Raise an error if the request failed
-        
-        data = response.json()  # Load JSON data
 
-        # Iterate over the 'data' list in the JSON
-        for course in data["data"]:
-            course_code = course["courseCode"]
-            section_details = course["courseDetails"]
-            seat_cap = course["defaultSeatCapacity"]
-            booked = course["totalFillupSeat"]
-            available_seat = course["availableSeat"]
-            if available_seat == "":
-                available_seat = 0
-            else:
-                available_seat = int(available_seat)
-
-            # Check for specific course and section (CSE321-[03])
-            if course_code == c_code and section_details == f"{c_code}-[{sec}]":
-                print(f"Course Code: {course_code}")
-                print(f"Section: {section_details}")
-                print(f"Seat Capacity: {seat_cap}")
-                print(f"Booked: {booked}")
-                print(f"Available: {available_seat}")
-
-                # If available seats are more than 0, send a notification
-                if available_seat > 0:
-                    if os.name == "nt":
-                        send_desktop_notification(available_seat) # Notify in Windows
-                    else:
-                        send_termux_notification(available_seat)  # Notify in Termux
+        def print_details():
+            print(f"Course Code: {course_code}")
+            print(f"Program: {program}")
+            print(f"Faculty: {faculty}")
+            print(f"Section: {section}")
+            print(f"Total Seat: {total_seat}")
+            print(f"Seat Booked: {seat_booked}")
+            print(f"Seat Remaining: {seat_remaining}")
+            if seat_remaining > 0:
+                print("Seats are available!")
+                if os.name == "nt":
+                    send_desktop_notification(seat_remaining) # Notify in Windows
                 else:
-                    print("No seats available at the moment.")
-                break  # Break after finding the section you're interested in
+                    send_termux_notification(seat_remaining)  # Notify in Termux
+            else:
+                print("No seats available at the moment.")
+        
+        if url_selected == 1:
+            response = requests.get(json_url)
+            response.raise_for_status()  # Raise an error if the request failed
+            data = response.json() # Load JSON data
+
+            # Iterate over the 'data' list in the JSON
+            for course in data["data"]:
+                course_code = course["courseCode"]
+                section = course["courseDetails"].split("-")[1][1:-1]
+                program = course["deptName"]
+                faculty = course["empShortName"]
+                total_seat = course["defaultSeatCapacity"]
+                seat_booked = course["totalFillupSeat"]
+                seat_remaining = course["availableSeat"]
+                if seat_remaining == "":
+                    seat_remaining = 0
+                else:
+                    seat_remaining = int(seat_remaining)
+                # Check for the course and section you are interested in
+                if course_code == c_code and section == sec:
+                    print_details()
+                    break  # Stop after finding the desired course
+        else:
+            # Fetch the page content
+            response = requests.get(usis_url)
+            response.raise_for_status()  # Check for request success
+            # Parse the HTML content
+            soup = BeautifulSoup(response.content, "html.parser")
+            # Find the table with the course data
+            table = soup.find("table", {"id": "customers"})
+        
+            # Iterate through each row (tr)
+            for row in table.find_all("tr")[1:]:  # Skip the header
+                cols = row.find_all("td")
+                course_code = cols[1].text.strip()
+                program = cols[2].text.strip()
+                faculty = cols[3].text.strip()
+                section = cols[5].text.strip()
+                total_seat = int(cols[7].text.strip())
+                seat_booked = int(cols[8].text.strip())
+                seat_remaining = int(cols[9].text.strip())
+                # Check for the course and section you are interested in
+                if course_code == c_code and section == sec:
+                    print_details()
+                    break  # Stop after finding the desired course
     
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -75,6 +107,13 @@ c_code = input("Enter Course Code: ").upper()
 sec = input("Enter Section: ")
 if len(sec) == 1:
     sec = '0' + sec
+
+print("\nWhich site would you like to choose to be updated from? :")
+print("1. Eniamza's Usis2Global. Or,")
+print("2. usis.bracu.ac.bd/academia/")
+url_selected = int(input("(Enter 1 or 2): "))
+while url_selected not in [1, 2]:
+    url_selected = int(input("Please enter a valid option (1 or 2): "))
 
 schedule.every(1).minutes.do(check_class)
 check_class()
